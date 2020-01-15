2ff82f1cb3d3493a4becfcd0f2c31682ce1ce045
Dee1 <deepakprasad119@gmail.com>
2020-01-15 14:34:44 +0530

    counting vallyes

--- IntrvPrograms/CountingValleys.py
+++ IntrvPrograms/CountingValleys.py
@@ -18,6 +18,7 @@
 # Sample Output
 # 1
 
+#!/bin/python3
 
 import math
 import os
@@ -25,56 +26,34 @@ import random
 import re
 import sys
 
+
+s = "UDDDUDUU"
+n = 8
 # Complete the countingValleys function below.
 
 sea_level = 0
 U = 1
 D = -1
-
-n = 8
-s = "UDDDUDUU"
-
-
-list1 = list(s)
-list2 = []
-print(len(list(s)))
-
-
-for i in range(len(s)):
-
-    if s[i] == "U":
-        list2.append("1")
-    elif s[i] == "D":
-        list2.append("-1")
-    else:
-        pass
-
-print(list2)
-
+list1 = []
 x = 0
 counter = 0
-for i in range(len(list2)):
-    x = x + int(list2[i])
-    #print(x - int(list2[i]))
-    if x - int(list2[i]) < 0 and x == 0:
-        counter = counter + 1
-
-print(counter)
 
 
 def countingValleys(n, s):
-    pass
+    x = 0
+    counter = 0
+    for i in range(len(s)):
+        if s[i] == "U":
+            list1.append(1)
+        else:
+            list1.append(-1)
+
+    for i in range(len(s)):
+        x = x + int(list1[i])
+        y = x - int(list1[i])
+        if x - int(list1[i]) < 0 and x == 0:
+            counter = counter + 1
+    return counter
 
 
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-#     n = int(input())
-
-#     s = input()
-
-#     result = countingValleys(n, s)
-
-#     fptr.write(str(result) + '\n')
-
-#     fptr.close()
+print(countingValleys(n, s))
